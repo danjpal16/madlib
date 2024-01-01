3d626cd5c44d27333b7fb09616e186f42a048283
danjpal16 <144719377+danjpal16@users.noreply.github.com>
2024-01-01 14:35:38 -0500

    added javascript

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-let adjective = prompt('Enter an adjective:')
-let noun = prompt('Enter a noun:')
-let verb = prompt('Enter a verb:')
-let adverb = prompt('Enter an adverb:')
+let color = prompt('Enter a color:')
+let size = prompt('Enter a size:')
+let color2 = prompt('Enter another color:')
+let lastname = prompt('Enter your last name:')
+let lastname2 = prompt('Enter your friends last name:')
 
-let story = `Once upon a time, in a ${adjective} ${noun}, a ${verb} ${noun} ${adverb} ${verb}.`
+let story = `'Twas the night before Christmas, and all through the ${color} house, not a creature was stirring, not even a ${size} mouse. The ${color2} stockings were hung by the ${lastname} with care, in hopes that St. ${lastname2} soon would be there.`
 
 let madLibOutputDiv = document.getElementById('madLibOutput')
 
